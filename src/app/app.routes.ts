bd231e1a4b61702d0c97f1aa81e551e2df0c0690
Jeroen van Vugt <jj.vanvugt@student.avans.nl>
2024-10-10 00:34:27 +0200

    Create dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,25 @@
 import { Routes } from '@angular/router';
 
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LedgerListComponent } from './components/ledger-list/ledger-list.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
-  { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
-  { path: '**', component: PageNotFoundComponent }
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'ledgers',
+    component: LedgerListComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/dashboard'
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
 ];
