6a83d6449b35a7458a1fbb00b81b9b52399894fb
Jeroen van Vugt <jj.vanvugt@student.avans.nl>
2024-12-29 22:45:59 +0100

    Add ledger (#4)
    * Update angular.json
    
    * Update dependencies
    
    * Add ledger
    
    * Update dashboard

---
+++ src/app/components/ledger-form/ledger-form.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-ledger-form',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './ledger-form.component.html'
+})
+export class LedgerFormComponent {
+  ledgerForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    description: new FormControl('')
+  });
+
+  get nameEmpty(): boolean {
+    const name = this.ledgerForm.get('name');
+    return name!.hasError('required') && name!.touched;
+  }
+}
