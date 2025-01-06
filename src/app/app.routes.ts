9ca5732451506550ce60c8d888ac24f084166d97
Jeroen van Vugt <jj.vanvugt@student.avans.nl>
2025-01-06 19:17:53 +0100

    Archive list (#9)
    * Refactor
    
    * Archive list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LedgerListComponent } from './components/ledger-list/ledger-list.component';
+import { ArchiveListComponent } from './components/archive-list/archive-list.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
@@ -12,6 +13,10 @@ export const routes: Routes = [
   {
     path: 'ledgers',
     component: LedgerListComponent
+  },
+  {
+    path: 'archive',
+    component: ArchiveListComponent
   },
   {
     path: '',
