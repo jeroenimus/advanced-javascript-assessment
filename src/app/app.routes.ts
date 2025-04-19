3fa73073cd47770a29ce1b0a06a7cc575d44a4df
Jeroen van Vugt <jj.vanvugt@student.avans.nl>
2025-04-19 20:53:25 +0200

    Add category list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LedgerListComponent } from './components/ledger-list/ledger-list.component';
 import { EntryListComponent } from './components/entry-list/entry-list.component';
+import { CategoryListComponent } from './components/category-list/category-list.component';
 import { ArchiveListComponent } from './components/archive-list/archive-list.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
@@ -18,6 +19,10 @@ export const routes: Routes = [
   {
     path: 'ledgers/:id',
     component: EntryListComponent
+  },
+  {
+    path: 'categories',
+    component: CategoryListComponent
   },
   {
     path: 'archive',
