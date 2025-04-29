dc7dfef8810a1530ceb28a865987928f58d8766b
Jeroen van Vugt <jj.vanvugt@student.avans.nl>
2025-04-29 20:09:10 +0200

    Hosting (#26)
    * Update .gitignore
    
    * Fix routing bug
    
    * Fix empty entry table
    
    * Fix category budget
    
    * Add hosting

--- src/app/pipes/balance.pipe.spec.ts
+++ src/app/pipes/balance.pipe.spec.ts
@@ -16,7 +16,7 @@ describe('BalancePipe', () => {
     expect(pipe.transform([])).toBe(0);
   });
 
-  it ('should return the correct total for only credit entries', () => {
+  it ('should return the correct total for credit entries only', () => {
     const entries: Entry[] = [
       { id: 'entry1', description: 'Entry 1', amount: 1, type: 'credit', categoryId: 'category1', ownerId: 'owner1', createdOn: new Date() },
       { id: 'entry2', description: 'Entry 2', amount: 2, type: 'credit', categoryId: 'category1', ownerId: 'owner1', createdOn: new Date() }
@@ -25,7 +25,7 @@ describe('BalancePipe', () => {
     expect(pipe.transform(entries)).toBe(3);
   });
 
-  it ('should return the correct total for only debit entries', () => {
+  it ('should return the correct total for debit entries only', () => {
     const entries: Entry[] = [
       { id: 'entry1', description: 'Entry 1', amount: 1, type: 'debit', categoryId: 'category1', ownerId: 'owner1', createdOn: new Date() },
       { id: 'entry2', description: 'Entry 2', amount: 2, type: 'debit', categoryId: 'category1', ownerId: 'owner1', createdOn: new Date() }
